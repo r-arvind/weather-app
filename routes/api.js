c31e043ed94105fe540d6e05deeb3e688155ee81
Arvind R <r.arvind@iiitb.org>
2019-02-02 15:27:41 +0530

    Added sample data

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var sampleData = require("./../db/sample.json");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send(`lat : ${req.query.lat} long : ${req.query.long}`);
+});
+router.get('/test', function(req, res, next) {
+  res.json(sampleData);
 });
 
 module.exports = router;
